a0b3a2b3eb6c6c197dbf89c14b82499eab6ff8fe
Débora Castro <deboracastro.pm@gmail.com>
2021-02-17 13:47:54 -0300

    feat: add validade empty field confirmation password for file SingUpController

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,10 +1,10 @@
-import { HttpResponse, HttpRequest} from '../protocols/http'
+import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../error/missing-param-error'
 import { badRequest } from '../helpers/httpHelper'
 
 export class SingUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
