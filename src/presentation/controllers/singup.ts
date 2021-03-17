35f29b275ec6a959061fd6386d899291aaa98c1a
Débora Castro <deboracastro.pm@gmail.com>
2021-03-17 12:02:56 -0300

    refactor: move error imports for a signle file

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,9 +1,8 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
-import { MissingParamError } from '../error/missing-param-error'
+import { MissingParamError, InvalidParamError } from '../error'
 import { badRequest, serverError } from '../helpers/httpHelper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
-import { InvalidParamError } from '../error/invalid-param-error'
 
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator
