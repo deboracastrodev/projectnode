b64e536d9439955336a27c41b13efbd52e58311f
Débora Castro <deboracastro.pm@gmail.com>
2021-04-01 17:54:17 -0300

    refactor: move configuration BCryptAdapter for makeSut

--- src/infra/cryptography/bcripty-adapter.spec.ts
+++ src/infra/cryptography/bcripty-adapter.spec.ts
@@ -1,12 +1,28 @@
 import bcrypt from 'bcrypt'
 import { BCryptAdapter } from './bcripty-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hash'))
+  }
+}))
+
+const salt = 12
+const makeSut = (): BCryptAdapter => {
+  return new BCryptAdapter(salt)
+}
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
-    const salt = 12
-    const sut = new BCryptAdapter(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    sut.encrypt('any_value')
+    await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  test('Should returns a hash on success', async () => {
+    const sut = makeSut()
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
